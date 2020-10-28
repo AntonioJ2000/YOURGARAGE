205bf048e336e8862d1a1b724e739d030bf2a493
Antonio Jimenez <antoniojl2000@hotmail.com>
2020-10-28 17:01:33 +0100

    Primera subida jajajaj

---
+++ app/src/main/java/com/anto/yourgarage/LogoActivity.java
@@ -0,0 +1,31 @@
+package com.anto.yourgarage;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        getSupportActionBar().hide();
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+
+
+}
